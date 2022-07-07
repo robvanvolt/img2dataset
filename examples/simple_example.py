d4d37a5c252a531ceac92d69f11b558d349105a9
robvanvolt <robvanvolt@gmail.com>
2022-07-07 20:25:03 +0200

    Added joblist

--- examples/simple_example.py
+++ examples/simple_example.py
@@ -1,25 +1,49 @@
 from img2dataset import download
 import shutil
 import os
+import time
 
-output_dir = os.path.abspath("bench")
+jobfolder = "joblist/"
+parts = range(64)
 
-if os.path.exists(output_dir):
-    shutil.rmtree(output_dir)
+os.makedirs(jobfolder, exist_ok=True)
 
-download(
-    processes_count=16,
-    thread_count=32,
-    url_list="../tests/test_files/test_10000.parquet",
-    image_size=256,
-    output_folder=output_dir,
-    output_format="files",
-    input_format="parquet",
-    url_col="URL",
-    caption_col="TEXT",
-    enable_wandb=True,
-    number_sample_per_shard=1000,
-    distributor="multiprocessing",
-)
+for part in parts:
+    if os.path.isfile(jobfolder + f"{part:02d}"):
+        ### Find the first unstarted job.
+        pass
+    else:
+        ### Create file when starting job.
+        now = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(time.time()))
+        with open(jobfolder + f"{part:02d}", "w") as f: 
+            f.write(f"Started job {part} at {now}.\n")
 
-# rm -rf bench
+
+        ### Append time when finishing job.
+        now = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(time.time()))
+        with open(jobfolder + f"{part:02d}", "a") as f: 
+            f.write(f"Finished job {part} at {now}.")
+
+# if __name__ == '__main__':
+#     output_dir = os.path.abspath("bench")
+
+#     if os.path.exists(output_dir):
+#         shutil.rmtree(output_dir)
+
+#     download(
+#         processes_count=16,
+#         thread_count=32,
+#         url_list="s3://s-laion/improved-aesthetics-laion-2B-en-subset/5/",
+#         image_size=512,
+#         output_folder=output_dir,
+#         output_format="webdataset",
+#         input_format="parquet",
+#         url_col="URL",
+#         caption_col="TEXT",
+#         enable_wandb=False,
+#         number_sample_per_shard=1000,
+#         save_additional_columns=["similarity","hash","punsafe","pwatermark","AESTHETIC_SCORE"],
+#         distributor="multiprocessing"
+#     )
+
+#     # rm -rf bench
